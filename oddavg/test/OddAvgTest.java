902d63a16c34546b3950eae9ecb184724cf67bad
Máté <dontirikk@gmail.com>
2017-04-18 09:44:46 +0200

    add a unit test for oddAverage function

---
+++ oddavg/test/OddAvgTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OddAvgTest {
+  @Test
+  void testOddAverage() {
+    ArrayList<Integer> listOfNumbers = new ArrayList<>();
+    for (int i = 1; i < 10; i++) {
+      listOfNumbers.add(i);
+    }
+    assertEquals(5.0, OddAvg.oddAverage(listOfNumbers));
+  }
+
+}

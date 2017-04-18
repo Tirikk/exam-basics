31a214db632c72ff07cc3f1c345adf00bfb2d7fd
Máté <dontirikk@gmail.com>
2017-04-18 11:19:30 +0200

    simplify deck constructor

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -7,14 +7,8 @@ public class Deck {
   Deck(int numberOfCards) {
     String[] colors = {"hearts", "spades", "diamonds", "clubs"};
     String[] values = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-    if (numberOfCards <= 4) {
-      for (int i = 0; i < numberOfCards; i++) {
-        cards.add(new Card(colors[i], values[(int) Math.random() * 13]));
-      }
-    } else {
-      for (int i = 0; i < numberOfCards; i++) {
-        cards.add(new Card(colors[numberOfCards % 4], values[(int) Math.random() * 13]));
-      }
+    for (int i = 0; i < numberOfCards; i++) {
+      cards.add(new Card(colors[numberOfCards % 4], values[(int) Math.random() * 13]));
     }
   }
 

be1123852f07da8a90e489e366bbc3f7587f1b24
Máté <dontirikk@gmail.com>
2017-04-18 11:33:20 +0200

    fix deck constructor, and add toString method

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -8,7 +8,7 @@ public class Deck {
     String[] colors = {"hearts", "spades", "diamonds", "clubs"};
     String[] values = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     for (int i = 0; i < numberOfCards; i++) {
-      cards.add(new Card(colors[numberOfCards % 4], values[(int) Math.random() * 13]));
+      cards.add(new Card(colors[i % 4], values[(int) Math.random() * 13]));
     }
   }
 
@@ -20,5 +20,25 @@ public class Deck {
     Card temp = cards.get(0);
     cards.remove(0);
     return temp;
+  }
+
+  public String toString() {
+    int noOfSpades = 0;
+    int noOfDiamonds = 0;
+    int noOfHearts = 0;
+    int noOfClubs = 0;
+    for (Card card : cards) {
+      if (card.color.equalsIgnoreCase("spades")) {
+        noOfSpades++;
+      } else if (card.color.equalsIgnoreCase("hearts")) {
+        noOfHearts++;
+      } else if (card.color.equalsIgnoreCase("diamonds")) {
+        noOfDiamonds++;
+      } else {
+        noOfClubs++;
+      }
+    }
+    return String.format("%d cards -  %d Clubs, %d Diamonds, %d Hearts, %d Spades", cards.size(), noOfClubs,
+            noOfDiamonds, noOfHearts, noOfSpades);
   }
 }

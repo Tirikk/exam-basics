20fd095dcc5c480b85f7c2ca4839759b9ed14118
Máté <dontirikk@gmail.com>
2017-04-18 10:11:04 +0200

    add methods for handling the 'no destination provided' case

--- copy/src/ArgumentHandler.java
+++ copy/src/ArgumentHandler.java
@@ -3,6 +3,8 @@ public class ArgumentHandler {
   static void checkArgs(String[] args) {
     if (args.length == 0) {
       Operations.printUsage();
+    } else if (args.length == 1) {
+      Operations.noDestination();
     }
   }
 }
--- copy/src/Operations.java
+++ copy/src/Operations.java
@@ -3,4 +3,8 @@ public class Operations {
   static void printUsage() {
     System.out.printf("Usage:\ncopy [source] [destination]");
   }
+
+  static void noDestination() {
+    System.out.println("No destination provided");
+  }
 }
